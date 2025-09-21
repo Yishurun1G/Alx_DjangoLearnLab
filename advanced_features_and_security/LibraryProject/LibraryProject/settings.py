fb66de4edd8d646f231918a2f2514a27c638d6a2
Yishurun Getachew <yishurung@gmail.com>
2025-09-21 17:44:31 +0300

    Update settings.py

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -1,10 +1,23 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'bookshelf',  # <- make sure this is added
+# DEBUG should be False in production
+DEBUG = False
+
+# Security headers
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = 'DENY'
+
+# Cookies over HTTPS only
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
+# Optional: Content Security Policy (CSP) via middleware
+# Install django-csp: pip install django-csp
+MIDDLEWARE = [
+    ...
+    'csp.middleware.CSPMiddleware',  # add CSP middleware
+    ...
 ]
-AUTH_USER_MODEL = 'bookshelf.CustomUser'
+
+CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'",)
+CSP_STYLE_SRC = ("'self'",)
