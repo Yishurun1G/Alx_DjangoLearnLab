5b6a2cc001fb6c0824b18c58561becfe235bfbfe
Yishurun Getachew <yishurung@gmail.com>
2025-09-21 21:35:30 +0300

    Update query_samples.py

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,6 +13,6 @@ def run_queries():
     books_in_library = library.books.all()
     print(f"Books in {library_name}:", list(books_in_library.values_list("title", flat=True)))
 
-    # Retrieve the librarian for a library
-    librarian = library.librarian
+    # Retrieve the librarian for a library (explicit query)
+    librarian = Librarian.objects.get(library=library)
     print(f"Librarian of {library_name}: {librarian.name}")
