0c53a7e644e1ad71a2428d01cf803fcd2490136e
Yishurun Getachew <yishurung@gmail.com>
2025-09-21 16:04:09 +0300

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -9,8 +9,19 @@ INSTALLED_APPS = [
 
     # Third-party apps
     'rest_framework',
-    'rest_framework.authtoken',  
+    'rest_framework.authtoken',   # Token auth app
 
     # Local apps
     'api',
 ]
+
+# Add DRF settings with TokenAuthentication
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
