449de74a3a7866ebd4d1f7d28a3ee03ae4d0893e
Yishurun Getachew <yishurung@gmail.com>
2025-09-21 22:38:20 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Book CRUD with permissions
+    # Book CRUD paths with permissions
     path("books/add/", views.add_book, name="add_book"),
     path("books/<int:pk>/edit/", views.edit_book, name="edit_book"),
     path("books/<int:pk>/delete/", views.delete_book, name="delete_book"),
