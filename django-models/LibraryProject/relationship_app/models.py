4061f2eb6da6d88592da479a896aee88651bd4c8
Yishurun Getachew <yishurung@gmail.com>
2025-09-21 21:32:29 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,25 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import User
-
-class Student(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    student_id = models.CharField(max_length=20, unique=True)
-
-    def __str__(self):
-        return self.user.username
 
 
-class Teacher(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.user.username
-
-
-class Library(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
@@ -27,8 +10,23 @@ class Library(models.Model):
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    library = models.ForeignKey(Library, on_delete=models.CASCADE, related_name="books")
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name="books")
 
     def __str__(self):
         return self.title
+
+
+class Library(models.Model):
+    name = models.CharField(max_length=150)
+    books = models.ManyToManyField(Book, related_name="libraries", blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name="librarian")
+
+    def __str__(self):
+        return self.name
